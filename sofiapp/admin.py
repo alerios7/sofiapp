112b6fab55abf1f78d1243ea6cbc7362ceacda5b
Alejandro Ríos <alerios7@gmail.com>
2016-10-18 19:08:46 -0300

    creacion modelos project, category y ProjectCategoryRelationship

--- sofiapp/admin.py
+++ sofiapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Category, Project, ProjectCategoryRelationship
+
 # Register your models here.
+
+class ProjectCategoryRelationshipInline(admin.TabularInline):
+    model = ProjectCategoryRelationship
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'text', 'project_type', 'created_date']})
+    ]
+    inlines = [ProjectCategoryRelationshipInline]
+
+#class CategoryAdmin(admin.ModelAdmin):
+#    inlines = [ProjectCategoryRelationshipInline]
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category)
--- sofiapp/models.py
+++ sofiapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+#One project can have many categories, and one category can have many projects
+class Category(models.Model):
+    category_name = models.CharField('Nombre categoría', max_length=200, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+# Project model
+class Project(models.Model):
+    PROJECT_TYPE = (
+        ('I', 'Iluminación'),
+        ('P', 'Paisajismo'),
+        ('M', 'Mixto'),
+    )
+
+    title = models.CharField('Título', max_length=200)
+    text = models.TextField('Descripción')
+    project_type = models.CharField('Tipo', max_length=1, choices=PROJECT_TYPE, default='I')
+    created_date = models.DateTimeField(default=timezone.now())
+    categories = models.ManyToManyField(Category, through='ProjectCategoryRelationship')
+
+    def __str__(self):
+        return self.title
+
+class ProjectCategoryRelationship(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)

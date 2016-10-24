1d74c2036beaf7508f8cc071afba73981099be0e
Alejandro Ríos <alerios7@gmail.com>
2016-10-24 20:13:05 -0300

    modificacion menor base.html y vista

--- sofiapp/views.py
+++ sofiapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Project, Category
+from .models import Project, Category, Type
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'sofiapp/index.html', {'categories': categories})
+    project_types = Type.objects.all()
+    return render(request, 'sofiapp/index.html', {'categories': categories, 'project_types': project_types})

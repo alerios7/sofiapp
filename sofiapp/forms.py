6c36bf2bd5f78b050b72a7b5b3ac610644bb6b96
Alejandro Ríos <alerios7@gmail.com>
2016-10-25 19:48:47 -0300

    añadir clases de bootstrap al formulario

--- sofiapp/forms.py
+++ sofiapp/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    from_email = forms.EmailField(label="Email de contacto", required=True)
+    subject = forms.CharField(label="Asunto", required=True)
+    message = forms.CharField(label="Mensaje", widget=forms.Textarea, required=True)
+
+    #this is to add bootstrap class form-control to form
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['from_email'].widget.attrs = {'class': 'form-control'}
+        self.fields['subject'].widget.attrs = {'class': 'form-control'}
+        self.fields['message'].widget.attrs = {'class': 'form-control'}
